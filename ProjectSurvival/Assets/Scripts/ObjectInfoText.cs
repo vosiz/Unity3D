672837e4912fd3ebae559cc6f5e572edfee7ba5a
Petr "Vošiz" Vošoust <petr.vosoust.ml@email.cz>
2017-04-15 16:21:57 +0200

    ProjectSurvival update - 3 simple object, distance
    ================================
    
    - added stone, grass, stick cube
    - calculationg distance from object

---
+++ ProjectSurvival/Assets/Scripts/ObjectInfoText.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectInfoText : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ ProjectSurvival/Assets/Scripts/PlayerCasting.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCasting : MonoBehaviour {
+
+    public float distance;
+    public Transform other;
+
+    // Update is called once per frame
+    void Update () {
+
+        RaycastHit hit;
+        Vector3 up = transform.TransformDirection(Vector3.forward);
+        if (Physics.Raycast(transform.position, up, out hit))
+        {
+            distance = hit.distance;
+        }
+
+    }
+}
+
+
+//static var DistanceFromTarget : float;
+
+//var ToTarget : float;
+
+//function Update()
+//{
+
+//    var hit : RaycastHit;
+
+//    if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), hit))
+//    {
+//        ToTarget = hit.distance;
+//        DistanceFromTarget = ToTarget;
+//    }
+//}
