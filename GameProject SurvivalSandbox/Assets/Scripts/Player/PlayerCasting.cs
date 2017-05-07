e1a3ab4397663e8b2604f6eefce1267ae2be5e96
Petr "Vošiz" Vošoust <petr.vosoust.ml@email.cz>
2017-05-07 22:14:06 +0200

    ProjectSurvival update - player cassting
    ================================
    
    - added player casting, so system will know distance between player and another object in map

---
+++ GameProject SurvivalSandbox/Assets/Scripts/Player/PlayerCasting.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCasting : MonoBehaviour {
+
+    static public float distance;
+
+
+    public GameObject player;
+
+
+    //Debug 
+    //[SerializeField]
+    //private Vector3 player_pos;
+    //public float debug_distance;
+
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // updater
+    void Update() {
+
+        RaycastHit hit;
+        Camera player_cam = GetComponentInChildren<Camera>();
+
+        //Debug.Log("Camera is: " + player_cam.ToString());
+        //player_pos = player.transform.position;
+
+        if (Physics.Raycast(player_cam.ScreenPointToRay(Input.mousePosition), out hit)) {
+            distance = hit.distance;
+        }
+    }
+}
